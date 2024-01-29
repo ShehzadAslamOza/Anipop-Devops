9f27bf4c496ae47bd266550787107aa7922f029c
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-01-29 20:51:26 +0500

    roboto font

--- src/PassPage/PassPageComponents/Card.js
+++ src/PassPage/PassPageComponents/Card.js
@@ -1,21 +1,36 @@
 import React from "react";
 import "./Card.css";
+import { useState } from "react";
 
 export default function Card(props) {
+  const [isMobile] = useState(window.innerWidth < window.innerHeight);
+
   return (
     <div className="card">
       <img src={props.img} class="card__image" alt="" />
       <h2 className="card__title card_elem">{props.title}</h2>
       <p className="card__description card_elem">{props.description}</p>
 
-      <button className="card__btn card_elem">
-        <span class="circle" aria-hidden="true">
-          <span class="icon arrow"></span>
-        </span>
-        <a href={props.link} class="button-text">
-          {props.action}
-        </a>
-      </button>
+      {!isMobile ? (
+        <button className="card__btn card_elem">
+          <div className="arrow-container">
+            <span class="circle" aria-hidden="true">
+              <span class="icon arrow"></span>
+            </span>
+            <a href={props.link} class="button-text">
+              {props.action}
+            </a>
+          </div>
+        </button>
+      ) : (
+        <button className="mobile-btn card__btn card_elem">
+          <div>
+            <a href={props.link} class="">
+              {props.action}
+            </a>
+          </div>
+        </button>
+      )}
     </div>
   );
 }
