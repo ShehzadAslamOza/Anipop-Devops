50b4e4e2177a1b527185bd5695f56200e233ec6d
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-05-06 18:46:39 +0500

    Devop Project

---
+++ K8s.tf
@@ -0,0 +1,66 @@
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "react-minikube-tf"
+  }
+}
+
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name = "anipop-deployement"
+    labels = {
+      test = "anipop-site"
+    }
+    namespace = "react-minikube-tf"
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        test = "anipop-site"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "anipop-site"
+        }
+      }
+
+      spec {
+        container {
+          image = "mhassaanf/project-devops:latest"
+          name  = "example"
+
+          resources {
+            requests = {
+              cpu  = "100m"  # Adjust based on observed usage
+              memory = "100Mi" # Adjust based on observed usage
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "example" {
+  metadata {
+    name = "anipop-service"
+    namespace = "react-minikube-tf"
+  }
+
+  spec {
+    selector = {
+      test = "anipop-site"
+    }
+
+    type = "NodePort"  # Expose the service on a random port on the Minikube node
+    port {
+      port        = 80  # Define the port within the cluster for your app (typically 80 for HTTP)
+      target_port = 80  # Map the service port to the container port where your app listens (usually 80)
+    }
+  }
+}
