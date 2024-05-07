41dc82253ac9952476d7c6f8a201cd985fb97d64
ShehzadAslamOza <shehzadaslamoza@gmail.com>
2024-05-07 05:59:38 +0500

    did changes

--- providers.tf
+++ providers.tf
@@ -1,3 +1,13 @@
-provider "minikube" {
-  kubernetes_version = "v1.28.3"
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.17.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+    config_path    = "C:\Users\shehzad\Desktop\Devops-Project\kubeconfig"
+    config_context = "minikube"
 }
