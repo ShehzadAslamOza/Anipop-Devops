701d8fe67cd02a0f0a23db17fb54af257014bcf8
Shehzad Aslam Oza <32712842+ShehzadAslamOza@users.noreply.github.com>
2024-05-07 05:11:46 +0500

    Update providers.tf
    to fix terraform deployment issue

--- providers.tf
+++ providers.tf
@@ -1,13 +1,12 @@
 terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.11.0"
-    }
-  }
+ required_providers {
+  kubernetes = {
+   source = "hashicorp/kubernetes"
+   version = "2.11.0"
+  }
+ }
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
+    # No config_path or config_context
 }
