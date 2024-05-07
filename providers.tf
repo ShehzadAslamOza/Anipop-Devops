0024f79530c527d51da212f83baa0651be6f851e
ShehzadAslamOza <shehzadaslamoza@gmail.com>
2024-05-07 05:47:25 +0500

    did changes

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
-terraform {
-        required_providers {
-        kubernetes = {
-        source = "hashicorp/kubernetes"
-        version = "2.11.0"
-        }
-    }
-}
-
-provider "kubernetes" {
-    # No config_path or config_context
+provider "minikube" {
+  kubernetes_version = "v1.28.3"
 }
