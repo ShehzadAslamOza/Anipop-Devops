cd36014cc897d4f1fc5af4002812f37320276c34
ShehzadAslamOza <shehzadaslamoza@gmail.com>
2024-05-07 05:18:43 +0500

    removed extra characters from providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,10 @@
 terraform {
- required_providers {
-  kubernetes = {
-   source = "hashicorp/kubernetes"
-   version = "2.11.0"
-  }
- }
+        required_providers {
+        kubernetes = {
+        source = "hashicorp/kubernetes"
+        version = "2.11.0"
+        }
+    }
 }
 
 provider "kubernetes" {
